f4943314355db18e77691afe2d71e99e0388409d
sabarinathan321 <sabarinathan935@gmail.com>
2019-12-25 18:54:30 +0530

    if condition were executed with condition style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,32 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import react from './image/react.PNG'
 import './App.css';
-
-function sabTest() {
-  let testingName = "nested Function"
-  return (
-    <h1>{testingName}</h1>
-  )
-}
-
 class App extends React.Component {
   render() {
-    let count = 5;
-    let name = {
-      firstName: 'Sabarinathan',
-      lastName: 'Ayyappan'
-    }
-    let styles = {
-      color: 'blue',
-      backgroundColor: 'gray'
-    }
+   let x = 30, y = 20
+   let trueStyle = {
+     color: 'green',
+     backgroundColor: 'gray'
+   }
+   let falseStyle = {
+    color: 'red',
+    backgroundColor: 'gray'
+  }
     return (<React.Fragment>
-      <h1>{count}</h1>
-      <h2>{count * count}</h2>
-      <h3 style={styles}>{name.firstName} 2 {name.lastName}"</h3>
-      {/* <h4 Color = 'blue'>blue color</h4> */}
-      {/* <h5 backgroundColor = {styles.backgroundColor}>background color</h5> */}
-      <h4>{sabTest()}</h4>
+  <h1 style = {x > y ? trueStyle : falseStyle}>{x}{'>'}{y}{" "}{x > y ? 'true' : 'false'}</h1>
     </React.Fragment>)
   }
 }
