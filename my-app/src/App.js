26996354e46bc18b6abeb6ec83b3ed587e221c18
sabarinathan321 <sabarinathan935@gmail.com>
2019-12-25 19:52:34 +0530

    added looping concepts and react bootstrap is added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import react from './image/react.PNG'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 class App extends React.Component {
   render() {
-   let x = 30, y = 20
-   let trueStyle = {
-     color: 'green',
-     backgroundColor: 'gray'
-   }
-   let falseStyle = {
-    color: 'red',
-    backgroundColor: 'gray'
-  }
+    let x = 30, y = 20, element = null;
+    let jsonArray = [
+      { firstName: 'sab', age: 21 },
+      { firstName: 'sound', age: 22 }
+    ]
+    let rowArray = [];
+    for (var i = 0; i < jsonArray.length; i++) {
+      var row = (
+        <tr>
+          <td>{jsonArray[i].firstName}</td>
+          <td>{jsonArray[i].age}</td>
+        </tr>
+      )
+      rowArray.push(row);
+    }
+    if (x > y) {
+      element = <h1>x is greater</h1>
+    }
+    else {
+      element = <h1>y is greater</h1>
+    }
     return (<React.Fragment>
-  <h1 style = {x > y ? trueStyle : falseStyle}>{x}{'>'}{y}{" "}{x > y ? 'true' : 'false'}</h1>
-    </React.Fragment>)
+      {element}
+      <table style={{ width: '60%' }} className='table'>
+        <thead className="thead-light">
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rowArray}
+        </tbody>
+      </table>
+    </React.Fragment>
+
+    )
   }
 }
 
