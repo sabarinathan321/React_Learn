dea3699819d4e02c1e3890d949b685e78957a600
sabarinathan321 <sabarinathan935@gmail.com>
2019-12-25 17:54:22 +0530

    JSX with added images

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import react from './image/react.PNG'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <h3>ReactJS:</h3>
+    <img src={react} width="120" height="120" alt="React Logo"/>
+    <p> React is a JavaScript library for creating User Interfaces, open sourced to the world  by Facebook and  Instagram team in 2013.<br/>
+    React’s main goal is to make development of UI components easy and modular.
+    It is intended to ease the process of building<br/> large applications using
+    data that changes over time.</p>
+  </React.Fragment>
   );
 }
 
